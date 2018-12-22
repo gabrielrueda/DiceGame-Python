2c0adf4c43901fc5f3c98b747c080b428b63fb11
Gabriel <gabriel.i.rueda@gmail.com>
2018-12-22 10:25:30 -0800

    Made Basics of the game

--- game.py
+++ game.py
@@ -1,1 +1,8 @@
-print("Choose a number from 1 to 6");
+import random
+computerChoice = random.randint(1,6)
+userChoice = input("Choose a number from 1 to 6 : ");
+
+if userChoice == computerChoice:
+    print("You guessed correctly!");
+else:
+    print("You guessed wrong.");
